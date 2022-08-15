a9141fc6f219833282a3ba06b24a390ad26cea1d
Levan D <97022558+Levan-D@users.noreply.github.com>
2022-08-15 21:27:09 +0400

    restructured button positions

--- my-app/src/components/Scheduler/Todo.js
+++ my-app/src/components/Scheduler/Todo.js
@@ -1,33 +1,34 @@
 /** @format */
-import { ACTIONS } from "./Scheduler";
+import { ACTIONS } from "./Scheduler"
 
 function Todo({ todo, toggle }) {
   return (
-    <div className="todoContainer">
-      <div
-        className="todoCheck"
-        onClick={() => {
-          toggle({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } });
-        }}
-      >
-        &#10004;
-      </div>
-      <div
-        className={`todo ${todo.complete ? "todoComplete" : "todoNotComplete"}`}
-      >
-        {todo.name}
-      </div>
-
-      <div
-        className="todoDelete"
-        onClick={() => {
-          toggle({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } });
-        }}
-      >
-        &#8211;
+    <div className="azazaz">
+      <div className="todoContainer">
+        <div className={`todo ${todo.complete ? "todoComplete" : "todoNotComplete"}`}>
+          {todo.name}
+        </div>
+        <div className="todoButtonsFlex">
+          <div
+            className="todoCheck"
+            onClick={() => {
+              toggle({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })
+            }}
+          >
+            &#10004;
+          </div>
+          <div
+            className="todoDelete"
+            onClick={() => {
+              toggle({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
+            }}
+          >
+            &#8211;
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Todo;
+export default Todo
