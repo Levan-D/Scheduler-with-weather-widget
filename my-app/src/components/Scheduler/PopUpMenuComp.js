daf56dcf72c229018e91694bca02e2440edf620a
Levan D <97022558+Levan-D@users.noreply.github.com>
2022-08-23 17:28:02 +0400

    added rename button

--- my-app/src/components/Scheduler/PopUpMenuComp.js
+++ my-app/src/components/Scheduler/PopUpMenuComp.js
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from "react";
-
+import React, { useRef, useEffect, useState } from "react";
 import "./popUpMenu.css";
 
 function PopUpMenuComp({ deleteFunc, coords, visibility }) {
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [confirmNameChange, setConfirmNameChange] = useState(false);
   const refOne = useRef(null);
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
@@ -21,19 +22,63 @@ function PopUpMenuComp({ deleteFunc, coords, visibility }) {
       onMouseLeave={visibility}
       style={{ top: coords.y + "px", left: coords.x + "px" }}
     >
-      <div className="popUpButton deleteButton" onClick={deleteFunc}>
-        <div className="icon-trash">
-          <div className="trash-lid"></div>
-          <div className="trash-container"></div>
-          <div className="trash-line-1"></div>
-          <div className="trash-line-2"></div>
-          <div className="trash-line-3"></div>
+      <div className="popUpWrapper">
+        <div
+          className="popUpButton"
+          onClick={(x) => {
+            setConfirmNameChange(!confirmNameChange);
+          }}
+        >
+          <div className="pencil"></div>
+        </div>
+        <div className="popUpButton"></div>
+        <div
+          className="popUpButton deleteButton"
+          onClick={(x) => {
+            setConfirmDelete(!confirmDelete);
+          }}
+        >
+          <div className="icon-trash">
+            <div className="trash-lid"></div>
+            <div className="trash-container"></div>
+            <div className="trash-line-1"></div>
+            <div className="trash-line-2"></div>
+            <div className="trash-line-3"></div>
+          </div>
         </div>
       </div>
-      <div className="popUpButton"></div>
-      <div className="popUpButton"></div>
+      {confirmDelete && (
+        <div className="confirmTab">
+          <div onClick={deleteFunc}>Confirm</div>
+        </div>
+      )}
+      {confirmNameChange && (
+        <div
+          className="confirmTab"
+          style={{
+            height: `auto`,
+            padding: `10px 0px`,
+            width: `auto`,
+          }}
+        >
+          <div>
+            <form className="nameChangeForm">
+              <input
+                type="text"
+                required
+                placeholder="Enter task here!"
+                // value={taskName}
+                // onChange={(e) => setTaskName(e.target.value)}
+              />
+              <input type="submit" value="Rename" />
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default PopUpMenuComp;
+
+// deleteFunc;
