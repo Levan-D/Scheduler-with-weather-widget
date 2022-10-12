d94f7d896dbee47d79bcef959a340ffd0e9c6515
Levan D <97022558+Levan-D@users.noreply.github.com>
2022-10-12 18:17:45 +0400

    s

--- src/components/Scheduler/Todo/TodoTask.js
+++ src/components/Scheduler/Todo/TodoTask.js
@@ -65,7 +65,7 @@ const TodoTask = ({ todo, name, dragging }) => {
       })
     );
   };
-  console.log(todoData);
+
   const handleRenameTodo = (e) => {
     e.preventDefault();
     if (!isLoggedIn) {
